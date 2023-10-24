2ef1dfad8fad210791cb021b9f3c24effc4cd155
Allen Gustrowsky <allen@may21.us>
2023-10-24 19:18:07 -0400

    fix duplicate margin

--- src/components/MessageFeed.jsx
+++ src/components/MessageFeed.jsx
@@ -7,7 +7,7 @@ const MessageFeed = (props) => {
         <div id="messages-container">
             <Paper sx={{ maxHeight: "350px", overflow: "scroll", maxWidth: "700px", margin: "15px auto 0", padding: "10px" }}>
                 {messages.map((msg, key) => {
-                    return <Alert severity="info" key={key} sx={{ maxWidth: "700px", margin: "0 auto", textAlign: "left", margin: "5px 0" }}>{msg}</Alert>
+                    return <Alert severity="info" key={key} sx={{ maxWidth: "700px", textAlign: "left", margin: "5px 0" }}>{msg}</Alert>
                 })}
             </Paper>
         </div>
